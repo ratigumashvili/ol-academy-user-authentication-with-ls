4a690c8660d199d7d3a86498a2c607d7e9859301
Rati <ratigumashvili@gmail.com>
2022-10-30 14:49:47 +0400

    Added small optimizations

--- src/components/EndScreen.jsx
+++ src/components/EndScreen.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 const EndScreen = ({ loggedInUser, setNavigation }) => {
+  const { name, surname } = loggedInUser[0];
   return (
     <>
       <h1>EndScreen</h1>
-      {loggedInUser && <h3>Hello, {loggedInUser[0]?.name}</h3>}
+      {loggedInUser && (
+        <h3>
+          Hello, {name} {surname}
+        </h3>
+      )}
       <button onClick={() => setNavigation("registration")}>
         Go home, your're drunk
       </button>
